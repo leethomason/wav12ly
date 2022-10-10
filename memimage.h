567a7e11125310ff8364ab1928eb6e603d0eee7b
Lee Thomason <leethomason@gmail.com>
2022-10-10 16:50:15 -0700

    add errors

--- memimage.h
+++ memimage.h
@@ -16,6 +16,12 @@ public:
     void addFile(const char* name, const void* data, int size, int table, int32_t e12);
     void addConfig(uint8_t font, uint8_t bc_r, uint8_t bc_g, uint8_t bc_b, uint8_t ic_r, uint8_t ic_g, int8_t ic_b);
     void dumpConsole();
+    int getNumFiles() const {
+        return numFile;
+    }
+    int getNumDirs() const {
+        return numDir;
+    }
 
     void write(const char* name);
     void writeText(const char* name);
